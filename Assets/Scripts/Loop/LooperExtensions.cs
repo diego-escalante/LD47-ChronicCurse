84a1f696e6aa956e5d0543f90108f24940e3ad8e
Diego Escalante <diego.escalante01@gmail.com>
2020-10-02 19:54:26 -0600

    Add loop breaking and charging

--- Assets/Scripts/Loop/LooperExtensions.cs
+++ Assets/Scripts/Loop/LooperExtensions.cs
@@ -7,16 +7,29 @@ public static class LooperExtensions {
     static private LoopController loop;
     
     public static void SubscribeToLoop(this ILooper iLooper) {
-        getLoop().Subscribe(iLooper);
+        LoopController loop = getLoop();
+        if (loop == null) {
+            Debug.LogError("Could not find LoopController to subscribe to!");
+            return;
+        }
+        loop.Subscribe(iLooper);
     }
 
     public static void UnsubscribeFromLoop(this ILooper iLooper) {
-        getLoop().Unsubscribe(iLooper);
+        LoopController loop = getLoop();
+        if (loop == null) {
+            return;
+        }
+        loop.Unsubscribe(iLooper);
     }
 
     private static LoopController getLoop() {
         if (loop == null) {
-            loop = GameObject.FindGameObjectWithTag("GameController").GetComponent<LoopController>();
+            GameObject gb = GameObject.FindGameObjectWithTag("GameController");
+            if (gb == null) {
+                return null;
+            }
+            loop = gb.GetComponent<LoopController>();
         }
         return loop;
     }
--- Assets/Scripts/Player/PlayerLooper.cs
+++ Assets/Scripts/Player/PlayerLooper.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class PlayerLooper : MonoBehaviour, ILooper {
     
+    public int maxBreakCharges = 1;
+    public int loopsToChargeBreak = 3;
+    
     private bool isLooping = true;
+    private int loopsToChargeBreakLeft;
     private Vector2 position;
+    private int breakCharges = 0;
 
     private void OnEnable() {
         this.SubscribeToLoop();
+        loopsToChargeBreakLeft = loopsToChargeBreak;
     }
 
     private void OnDisable() {
         this.UnsubscribeFromLoop();
+    }
+
+    private void Update() {
+        if (Input.GetButton("Action") && breakCharges > 0) {
+            BreakLoop();
+        }
     }
 
     public bool IsLooping() {
@@ -21,10 +33,23 @@ public class PlayerLooper : MonoBehaviour, ILooper {
 
     public void SetState() {
         position = transform.position;
+        isLooping = true;
     }
 
     public void Loop() {
         transform.position = position;
+        if (breakCharges < maxBreakCharges) {
+            loopsToChargeBreakLeft--;
+            if (loopsToChargeBreakLeft <= 0) {
+                breakCharges++;
+                loopsToChargeBreakLeft = loopsToChargeBreak;
+            }
+        }
+    }
+
+    private void BreakLoop() {
+        breakCharges--;
+        isLooping = false;
     }
 
 }
